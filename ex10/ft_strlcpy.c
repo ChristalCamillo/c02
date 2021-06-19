ea4c73560138c436088e6188a500ad96f862af8a
chrbrune <chrbrune@student.42sp.org.br>
2021-06-19 12:40:51 +0000

    c02

---
+++ ex10/ft_strlcpy.c
@@ -0,0 +1,23 @@
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	indice;
+	unsigned int	src_length;
+
+	src_length = 0;
+	indice = 0;
+	while (indice < size - 1 && src[indice] != '\0')
+	{
+		dest[indice] = src[indice];
+		indice ++;
+	}
+	while (indice < size)
+	{
+		dest[indice] = '\0';
+		indice++;
+	}
+	while (src[src_length] != '\0')
+	{
+		src_length++;
+	}
+	return (dest);
+}
